a1515f7aa2d581753e99695cc81818ac340e527e
rina <doroshukmaryna@gmail.com>
2024-01-15 00:47:57 +0100

    rewrote gameflow class

--- src/GameFlow.java
+++ src/GameFlow.java
@@ -10,21 +10,26 @@ class GameFlow {
     public Player whitePlayer;
     public Player blackPlayer;
     public Player currentPlayer;
-    private String gameRecord;
+    //    private String gameRecord;
     private int countMoves = 0;
 
     public GameFlow(Player whitePlayer, Player blackPlayer, Chessboard board) {
         this.whitePlayer = whitePlayer;
         this.blackPlayer = blackPlayer;
         this.board = board;
-        this.currentPlayer = whitePlayer;
-        this.gameRecord = "";
+//        this.gameRecord = "";
     }
+
     Command command;
     Command previousCommand;
+
     public void start() {
         board.FillInChessBoard();
-        while (!command.endTheGame()) {
+        currentPlayer = whitePlayer;
+        countMoves = 0;
+        command = null;
+        previousCommand = null;
+        while (!isGameOver(command)) {
             board.PrintChessBoard();
             boolean capture = false;
             boolean isWhiteTurn;
@@ -35,29 +40,60 @@ class GameFlow {
                 System.out.println("Current player: " + blackPlayer.getName());
                 isWhiteTurn = false;
             }
-
             command = currentPlayer.getCommand(board);
+            if (command instanceof Move) {
+                previousCommand = command;
+                Move move = (Move) command;
+                Piece piece = move.getPiece();
+                int[] source = move.getSource();
+                int[] destination = move.getDestination();
 
+                if (piece.getPieceType() && piece.isWhite == currentPlayer.isWhite() && piece.canMove(destination, board.getBoard())) {
+                    if (board.getPiece(destination).getPieceType()) {
+                        board.getPiece(destination).setKilled(true);
+                        capture = true;
+                    }
+                    board.movePiece(source, destination);
+                } else if (!piece.getPieceType()) {
+                    System.out.println("No piece there. Try again.");
+                    continue;
+                } else if (piece.isWhite != currentPlayer.isWhite()) {
+                    System.out.println("That's not your piece. Try again.");
+                    continue;
+                } else if (!piece.canMove(destination, board.getBoard())) {
+                    System.out.println("You can't move there. Try again.");
+                    continue;
+                } else {
+                    System.out.println("Invalid move. Try again.");
+                    continue;
+                }
+                currentPlayer = (currentPlayer == whitePlayer) ? blackPlayer : whitePlayer;
+                if (isWhiteTurn) {
+                    countMoves++;
+                }
+            } else if (command == null){
+                System.out.println("Invalid move. Try again.");
+            } else if (command instanceof RestartCommand) {
+                command.endTheGame();
+                start();
+            }
+            else {
+                command.endTheGame();
+            }
         }
     }
-//    public boolean isGameOver (Command command){
-//        if (countMoves == 50) {
-//            System.out.println("Draw by 50 moves rule.");
-//            return true;
-//        } else if (command instanceof QuitCommand) {
-//            System.out.println("Game over.");
-//            return true;
-//        } else if (command instanceof DrawCommand) {
-//            System.out.println("Draw.");
-//            return true;
-//        } else if (command instanceof RestartCommand) {
-//            System.out.println("Restarting the game.");
-//            return true;
-//        }
-//        return false;
-//        //TODO: implement later
-//        //check if it's not a checkmate
-//        //check if it's not a stalemate
-//    }
+    public boolean isGameOver(Command command){
+        if (countMoves == 50) {
+            System.out.println("Draw by 50 moves rule.");
+            return true;
+        } else if (command instanceof QuitCommand || command instanceof DrawCommand || command instanceof RestartCommand) {
+            return true;
+        }
+        return false;
+        //TODO: implement later
+        //check if it's not a checkmate
+        // check if it's not a stalemate
+    }
 }
+
 
